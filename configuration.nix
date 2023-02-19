31e19da96c28632523f55551c88bc121c7fb5bf1
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2023-02-19 03:13:13 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,10 +2,11 @@
   packages we use and setups for:
    * - plasma
    * - wired (i3-wm custom setup */ 
+#####################################################################
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
+#####################################################################
 { config, pkgs, ... }:
 
 {
