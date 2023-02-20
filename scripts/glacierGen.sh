ebd4050b119f8282f9fd5f2797c4a7a9a63a4718
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2023-02-20 04:15:21 -0600

    Create glacierGen.sh

---
+++ scripts/glacierGen.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env nix-shell
+set -euo pipefail
+###################
+ns="nightshade"
+tag="gen-21.3"
+gen=23
+label="$gen - $ns $tag - $(date)"
+####################
+# - functions
+####################
+notify () {
+	# sends notifications
+	local n1="GlacierGen"
+	local n2="New nightshadeNix generation created. Restart computer for changes to take effect."
+	notify-desktop "$n1" "$n2"
+}
+create_generation () {
+	# creates a new Nix generation with custom label.
+	# We're in nightshade23.
+	echo "nightshadeNix generation starting..."
+	sleep 1
+	echo "Preparation: $NIXOS_LABEL"
+	sleep 1
+	sudo NIXOS_LABEL=$label nixos-rebuild switch
+	sleep 1
+	echo "New nightshadeNix generation created. You'll need to restart your machine for changes to take effect."
+	sleep 1
+}
+main () {
+	# main function
+	create_generation
+	notify
+	exit
+}
+###############################
+# script entry
+###############################
+main
