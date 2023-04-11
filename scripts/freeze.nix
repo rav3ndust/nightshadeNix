a5f6c637699a241f53fd962f396d644b7cc8ed67
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2023-04-11 03:13:56 -0500

    Update freeze.nix

--- scripts/freeze.nix
+++ scripts/freeze.nix
@@ -1,5 +1,5 @@
 # freeze.nix
-#	Freeze automatically creates derivations from Git repositories.
+#	Freeze automatically creates Nix derivations from Git repositories.
 {
 	inputs = {
 		nixpkgs = {
@@ -17,23 +17,39 @@
 	};
 	# create the freeze script.
 	shellScript = ''
+		# - - - 
 		# input for derivation information
+		# - - - 
 		repositoryName = readInput "Enter the name of the git repo: "
 		brachOrTag = readInput "Enter the branch or tag of the git repo: "
 		outputDirectory = readInput "Enter the directory you'd like to store the derivation in: "
+
+		# - - - 
 		# clone the repo
+		# - - - 
 		echo "Cloning $repositoryName now..." && sleep 1
 		git clone "$repositoryName" "$outputDirectory" 
+		
+		# - - - 
 		# begin the derivation creation
+		# - - - 
 		echo "Creating new derivation..." && sleep 1
 		nix-instantiate --flake "$outputDirectory"
+		
+		# - - - 
 		# add files from the repo to the derivation
+		# - - - 
 		nix-store add-outputs --recursive "$outputDirectory"
+		
+		# - - - 
 		# now we set the version of the derivation to match that of the git repo.
+		# - - - 
 		nix-set-version --output-name "$outputDirectory" "$branchOrTag"
+		
+		# - - - 
 		# now we build the derivation
+		# - - - 
 		echo "Building the new derivation..." && sleep 1
 		nix-build "$outputDirectory"
 		'';
 }
-
